f02d4d7ea28cb43458781bdd4540ed23215e7d75
Yevheniia Maksymiv <evgeniamaksymiv@gmail.com>
2022-07-29 22:41:40 +0300

    added grid layout to Home component

--- client/src/pages/Home/index.js
+++ client/src/pages/Home/index.js
@@ -1,9 +1,19 @@
+import { Grid, Paper } from "@mui/material";
+
 const Home = () => {
   return (
-    <>
-      Home
-    </>
-  )
+    <Grid container spacing={2} marginTop={2}>
+      <Grid item xs={12}>
+        <Paper>Filters section</Paper>
+      </Grid>
+      <Grid item xs={12} md={8}>
+        <Paper>List of movies</Paper>
+      </Grid>
+      <Grid item xs={12} md={4}>
+        <Paper>Selected movies</Paper>
+      </Grid>
+    </Grid>
+  );
 };
 
 export default Home;
