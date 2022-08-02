48df66c3185dc2103b4e68b34a3aadda5f62c06d
Yevheniia Maksymiv <evgeniamaksymiv@gmail.com>
2022-08-02 19:47:44 +0300

    fixed padding of CardContent, added handle click IconButton

--- client/src/components/MovieCard/index.js
+++ client/src/components/MovieCard/index.js
@@ -8,24 +8,35 @@ import {
   MenuItem,
 } from '@mui/material';
 import MoreVertIcon from '@mui/icons-material/MoreVert';
+import { useState } from 'react';
 
 const ITEM_HEIGHT = 48;
 
 const MovieCard = () => {
+  const [anchorEl, setAnchorEl] = useState(null);
+  const open = Boolean(anchorEl);
+  const handleClick = (event) => {
+    setAnchorEl(event.currentTarget);
+  };
+  const handleClose = () => {
+    setAnchorEl(null);
+  };
+
   return (
     <Card sx={{ maxWidth: 225, position: 'relative' }}>
       <IconButton
         aria-label="settings"
         aria-haspopup="true"
         sx={{ position: 'absolute', top: '0', right: '0' }}
-        onClick={() => {}}
+        onClick={handleClick}
       >
         <MoreVertIcon />
       </IconButton>
       <Menu
         id="long-menu"
-        open={false}
-        onClose={() => {}}
+        anchorEl={anchorEl}
+        open={open}
+        onClose={handleClose}
         PaperProps={{
           style: {
             maxHeight: ITEM_HEIGHT * 4.5,
@@ -42,7 +53,7 @@ const MovieCard = () => {
         image="https://www.themoviedb.org/t/p/w220_and_h330_face/neMZH82Stu91d3iqvLdNQfqPPyl.jpg"
         alt="Movie's baner"
       />
-      <CardContent>
+      <CardContent sx={{ '&:last-child': { paddingBottom: '16px' } }}>
         <Typography variant="h6">The Lost City</Typography>
         <Typography variant="body2" color="text.secondary">
           Mar 24, 2022
