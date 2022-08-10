b9424383c63793472ca60bac9f4b8e239e3a22d6
Yevheniia Maksymiv <evgeniamaksymiv@gmail.com>
2022-08-10 19:02:49 +0300

    added ApolloProvider

--- client/src/App.js
+++ client/src/App.js
@@ -2,26 +2,34 @@ import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import { CssBaseline, Container, Box } from '@mui/material';
 import Navigation from './components/Navigation';
 import { Settings, Home, Recommend } from './pages';
+import { ApolloClient, InMemoryCache, ApolloProvider } from '@apollo/client';
 
 function App() {
+  const client = new ApolloClient({
+    uri: 'http://localhost:4000/',
+    cache: new InMemoryCache(),
+  });
+
   return (
-    <BrowserRouter>
-      <CssBaseline />
-      <Navigation />
-      <Box
-        sx={{
-          backgroundColor: (theme) => theme.palette.grey[100],
-        }}
-      >
-        <Container maxWidth="xl">
-          <Routes>
-            <Route path="/" element={<Home />} />
-            <Route path="settings" element={<Settings />} />
-            <Route path="settings" element={<Recommend />} />
-          </Routes>
-        </Container>
-      </Box>
-    </BrowserRouter>
+    <ApolloProvider client={client}>
+      <BrowserRouter>
+        <CssBaseline />
+        <Navigation />
+        <Box
+          sx={{
+            backgroundColor: (theme) => theme.palette.grey[100],
+          }}
+        >
+          <Container maxWidth="xl">
+            <Routes>
+              <Route path="/" element={<Home />} />
+              <Route path="settings" element={<Settings />} />
+              <Route path="settings" element={<Recommend />} />
+            </Routes>
+          </Container>
+        </Box>
+      </BrowserRouter>
+    </ApolloProvider>
   );
 }
 
