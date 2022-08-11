0c0c64c14509d0a64d359c449ef72aba1234837d
Yevheniia Maksymiv <evgeniamaksymiv@gmail.com>
2022-08-11 17:27:34 +0300

    added API_BASE_URL

--- server/src/config/index.js
+++ server/src/config/index.js
@@ -1,7 +1,9 @@
 const API_KEY = 'a600783f37c1be45124ca3ebfc159d45';
 const IMAGE_BASE_PATH = 'https://image.tmdb.org/t/p/w300';
+const API_BASE_URL = 'https://api.themoviedb.org/3/';
 
 module.exports = {
   API_KEY,
-  IMAGE_BASE_PATH
+  IMAGE_BASE_PATH,
+  API_BASE_URL
 }
