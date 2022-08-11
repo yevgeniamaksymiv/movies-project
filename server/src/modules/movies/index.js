4e7785d87a16b34b09de9c8ee5e8acbbf8918ef2
Yevheniia Maksymiv <evgeniamaksymiv@gmail.com>
2022-08-11 19:13:43 +0300

    added argument page to schema

--- server/src/modules/movies/index.js
+++ server/src/modules/movies/index.js
@@ -2,9 +2,9 @@ const axios = require('axios');
 const { Movies } = require('./entities/Movies');
 const { API_KEY, API_BASE_URL } = require('../../config');
 
-const getPopular = async () => {
+const getPopular = async (page) => {
   const result = await axios.get(
-    `${API_BASE_URL}movie/popular?api_key=${API_KEY}&language=en-US&page=1`
+    `${API_BASE_URL}movie/popular?api_key=${API_KEY}&language=en-US&page=${page}`
   );
   return new Movies(result.data);
 }
