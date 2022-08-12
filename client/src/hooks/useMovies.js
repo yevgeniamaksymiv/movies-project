fe1919e9ca08af07ce68dab0903fb7415a37a9a4
Yevheniia Maksymiv <evgeniamaksymiv@gmail.com>
2022-08-12 20:55:20 +0300

    created custom hook  for selecting, deleting movies

---
+++ client/src/hooks/useMovies.js
@@ -0,0 +1,24 @@
+import { useState, useCallback } from 'react';
+
+const MAX_SELECTED_MOVIES = 20;
+
+export const useMovies = () => {
+  const [selectedMovies, setSelectedMovies] = useState([]);
+
+  const selectMovie = useCallback((movie) => {
+    if (!selectedMovies.find(({ id }) => id === movie.id) 
+      && selectedMovies.length < MAX_SELECTED_MOVIES) {
+        setSelectedMovies([movie, ...selectedMovies])
+    }
+  }, [selectedMovies]);
+
+  const deleteMovie = useCallback((movie) => {
+    setSelectedMovies(selectedMovies.filter(({ id }) => id !== movie.id))
+  }, [selectedMovies]);
+
+  return {
+    selectedMovies,
+    selectMovie,
+    deleteMovie
+  }
+}
