152676cd5f41e9ac90c6319efb2b97cd74adc2c5
Yevheniia Maksymiv <evgeniamaksymiv@gmail.com>
2022-08-16 21:32:36 +0300

    added Form wrapper with error handler

--- client/src/components/SelectedMoviesForm/index.js
+++ client/src/components/SelectedMoviesForm/index.js
@@ -1,20 +1,50 @@
 import { Paper, TextField, IconButton } from '@mui/material';
 import CheckIcon from '@mui/icons-material/Check';
+import { Form, Field } from 'react-final-form';
 
-export const SelectedMoviesForm = () => {
+export const SelectedMoviesForm = ({ onSubmit }) => {
   return (
-    <Paper
-      sx={{
-        padding: '8px',
-        display: 'flex',
-        alignItems: 'center',
-        justifyContent: 'space-between',
+    <Form
+      onSubmit={onSubmit}
+      validate={(values) => {
+        const errors = {};
+        if (!values.listName) {
+          errors.listName = 'Required';
+        }
+        if (/\s/.test(values.listName)) {
+          errors.listName = 'No whitespace';
+        }
+        return errors;
       }}
-    >
-      <TextField sx={{ width: '85%' }} label="Put the list name" size="small" />
-      <IconButton color="primary">
-        <CheckIcon />
-      </IconButton>
-    </Paper>
+      render={({ handleSubmit }) => (
+        <form onSubmit={handleSubmit}>
+          <Paper
+            sx={{
+              padding: '8px',
+              display: 'flex',
+              alignItems: 'center',
+              justifyContent: 'space-between',
+            }}
+          >
+            <Field name="listName">
+              {({ input, meta }) => (
+                <>
+                  <TextField
+                    sx={{ width: '65%' }}
+                    label="Put the list name"
+                    size="small"
+                    {...input}
+                    />
+                    {meta.error && meta.touched && <span>{meta.error}</span>}
+                </>
+              )}
+            </Field>
+            <IconButton type="submit" color="primary">
+              <CheckIcon />
+            </IconButton>
+          </Paper>
+        </form>
+      )}
+    />
   );
 };
