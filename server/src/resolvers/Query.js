1f48cd3df5ad62eb137bcf6e01ad692e9893d18d
Yevheniia Maksymiv <evgeniamaksymiv@gmail.com>
2022-08-18 20:50:58 +0300

    got recomended movies by ids

--- server/src/resolvers/Query.js
+++ server/src/resolvers/Query.js
@@ -1,10 +1,19 @@
-const { getPopular } = require('../modules/movies');
+const { getPopular, getDetails } = require('../modules/movies');
+const { Movie } = require('../modules/movies/entities/Movie');
 
 async function movies(parent, args) {
   const data = await getPopular(args.page);
   return data;
 }
 
+async function moviesByIds(parent, { ids }) {
+  const requests = ids.map((id) => getDetails(id));
+  const data = await Promise.all(requests);
+  const movies = data.map((movie) => new Movie(movie.data));
+  return movies;
+}
+
 module.exports = {
-  movies
+  movies,
+  moviesByIds
 };
