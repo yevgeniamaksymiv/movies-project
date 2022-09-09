02fb19e6059406c4e66ebf692ab30f0bb018dbe2
Yevheniia Maksymiv <evgeniamaksymiv@gmail.com>
2022-09-09 17:49:16 +0300

    added env variable

--- server/src/modules/movies/index.js
+++ server/src/modules/movies/index.js
@@ -1,17 +1,19 @@
 const axios = require('axios');
 const { Movies } = require('./entities/Movies');
-const { API_KEY, API_BASE_URL } = require('../../config');
+const { API_BASE_URL } = require('../../config');
+
+require('dotenv').config();
 
 const getPopular = async (page, language) => {
   const result = await axios.get(
-    `${API_BASE_URL}movie/popular?api_key=${API_KEY}&language=${language}&page=${page}`
+    `${API_BASE_URL}movie/popular?api_key=${process.env.REACT_APP_API_KEY}&language=${language}&page=${page}`
   );
   return new Movies(result.data);
 }
 
 const getDetails = (id, language) => {
   return axios.get(
-    `${API_BASE_URL}movie/${id}?api_key=${API_KEY}&language=${language}`
+    `${API_BASE_URL}movie/${id}?api_key=${process.env.REACT_APP_API_KEY}&language=${language}`
   );
 }
 
